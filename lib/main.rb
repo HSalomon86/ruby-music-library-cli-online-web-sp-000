2ac4345cb2b54e6b43c2dff419c4e1fbbb71ac9b
Hendrick Salomon <hsalomon86@gmail.com>
2020-03-29 17:03:46 +0000

    Automatically backed up by Learn

--- lib/main.rb
+++ lib/main.rb
@@ -37,11 +37,15 @@ class MusicLibraryController
        end
      end
 
+     def artists
+       Artist.all.each.with_index(1) do |a, i|
+         puts "#{i}. #{a}"
      def list_songs
         Song.all.each_with_index do |song, index|
-        puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+        puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
        end
      end
+
 
        def list_artists
          #prints all artists in the music library in a numbered list (alphabetized by artist name
