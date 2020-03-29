16bfa31cf1dd051cde92f471ae33455b66bff772
Hendrick Salomon <hsalomon86@gmail.com>
2020-03-29 17:22:53 +0000

    Automatically backed up by Learn

--- lib/main.rb
+++ lib/main.rb
@@ -104,20 +104,37 @@ class MusicLibraryController
          end
        end
 
-       def play_song
-         puts "Which song number would you like to play?"
-         list_of_songs =  Song.all.sort{ |a, b| a.name <=> b.name }
-
-         input = gets.strip.to_i
-         #if (input > 0) && (input <= list_of_songs.size)
-         #if (input >= 1) && (input <= list_of_songs.size)
-         if (1..Song.all.length).include?(input)
-           song = list_of_songs[input+2]
-           puts "Playing #{song.name} by #{song.artist.name}"
-         else
-           nil
+       def song_array
+         sorted_library = self.library.sort_by {|song|song.name}
+         sorted_library.collect do |song|
+           "#{sorted_library.index(song) + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
          end
        end
+
+       def play_song
+  puts "Which song number would you like to play?"
+  song_names = self.song_array
+  user_input = gets.chomp.to_i
+  if user_input > 0 && user_input <= self.library.size
+    chosen_input = song_names[user_input - 1]
+    chosen_input = name_extractor(chosen_input)[1]
+    song = Song.find_by_name(chosen_input)
+    puts "Playing #{song.name} by #{song.artist.name}" unless song == nil
+  end
+      #  def play_song
+      #    puts "Which song number would you like to play?"
+      #    list_of_songs =  Song.all.sort{ |a, b| a.name <=> b.name }
+       #
+      #    input = gets.strip.to_i
+      #    #if (input > 0) && (input <= list_of_songs.size)
+      #    #if (input >= 1) && (input <= list_of_songs.size)
+      #    if (1..Song.all.length).include?(input)
+      #      song = list_of_songs[input+2]
+      #      puts "Playing #{song.name} by #{song.artist.name}"
+      #    else
+      #      nil
+      #    end
+      #  end
 
 
 end
