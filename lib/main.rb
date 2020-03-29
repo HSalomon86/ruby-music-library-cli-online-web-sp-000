a7bba56098b51124b568a57fd198c7c27970dd38
Hendrick Salomon <hsalomon86@gmail.com>
2020-03-29 17:00:55 +0000

    Automatically backed up by Learn

--- lib/main.rb
+++ lib/main.rb
@@ -39,7 +39,7 @@ class MusicLibraryController
 
      def list_songs
         Song.all.each_with_index do |song, index|
-        puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+        puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
        end
      end
 
